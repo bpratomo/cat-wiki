c3366eb790ee7b730824b387b91f8d887f8cbc30
Budi Pratomo <bpratomo@myrsm.nl>
2021-02-08 17:17:36 +0100

    Static mobile page done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,35 @@
 module.exports = {
-  purge: ['./src/**/*.html', './src/**/*.ts', './projects/**/*.html', './projects/**/*.ts'],
+  purge: [
+    "./src/**/*.html",
+    "./src/**/*.ts",
+    "./projects/**/*.html",
+    "./projects/**/*.ts",
+  ],
   theme: {
     extend: {
-      backgroundImage: theme => ({
-        'hero': "url('assets/HeroImagesm.png')",
-        'footer-texture': "url('/img/footer-texture.png')",
-      })
+      spacing: {
+        "1/2": "50%",
+        "1/3": "33.333333%",
+        "2/3": "66.666667%",
+        "1/4": "25%",
+        "2/4": "50%",
+        "3/4": "75%",
+        "1/5": "20%",
+        "2/5": "40%",
+        "3/5": "60%",
+        "4/5": "80%",
+        "1/6": "16.666667%",
+        "2/6": "33.333333%",
+        "3/6": "50%",
+        "4/6": "66.666667%",
+        "5/6": "83.333333%",
+      },
+      backgroundImage: (theme) => ({
+        hero: "url('assets/HeroImagesm.png')",
+        "footer-texture": "url('/img/footer-texture.png')",
+      }),
     },
   },
   variants: {},
   plugins: [],
- }
+};
